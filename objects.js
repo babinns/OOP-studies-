da91a9f6085b1fb70e47010e16a106a592f7c9c5
babinns <havenamehum@gmail.com>
2023-01-04 12:10:27 -0300

    object oriented programming studies

---
+++ objects.js
@@ -0,0 +1,10 @@
+const person = {
+    name: "Carla",
+    age: 18,
+    occupation: "saleswoman"
+}
+
+//accessing info
+
+console.log(`The person's name is ${person.name} and they are ${person.age} years old` );
+
